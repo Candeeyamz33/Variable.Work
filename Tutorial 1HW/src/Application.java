ff84af283aab0ce9e4afc8de2a0513ca8ef2f2e4
Nikisha Scott <nikisha.scott@yahoo.com>
2019-01-11 08:36:24 -0600

    Committing additional variable Java work

---
+++ Tutorial 1HW/src/Application.java
@@ -0,0 +1,30 @@
+
+public class Application {
+
+	public static void main(String[] args) {
+		int myNumber=88;
+		
+		short myShort=789;
+		long myLong=9546;
+				
+		double myDouble=7.978;
+		float myFloat=456.8f;
+		
+		char myChar='y' ;
+		boolean myBoolean=false;
+		
+		byte  
+		myByte=127;
+		
+		
+		System.out.println(myNumber);
+		System.out.println(myShort);
+		System.out.println(myLong);
+		System.out.println(myDouble);
+		System.out.println(myFloat);
+		System.out.println(myChar);
+		System.out.println(myBoolean);
+		System.out.println(myByte);
+	}
+
+}
